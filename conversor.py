ad4c75612cd5cc457af9c34c0472e405f5e3219e
Christopher Suquillo <csuquillo@est.ups.edu.ec>
2025-10-22 18:51:04 -0500

    Implementado actualización de tasas. Close #1

--- conversor.py
+++ conversor.py
@@ -1,5 +1,6 @@
 import json
 from datetime import datetime
+import random
 
 def cargar_tasas(ruta):
     """Lee un archivo JSON y retorna un objeto"""
@@ -23,9 +24,21 @@ def registrar_transaccion(producto, precio_convertido, moneda, ruta_log):
         # Escribir una linea nieva en el archivo de registro
         archivo.write(f"{fecha} | {producto}: {precio_convertido:.2f} {moneda}\n")
 
+def actualizar_tasas(ruta):
+    # Simular API: Cambiar tasas aleatoriamente ±2%
+    with open(ruta, "r+") as archivo:
+        tasas = json.load(archivo)
+        for moneda in tasas["USD"]:
+            tasas["USD"][moneda] *= 0.98 + (0.04 * random.random())
+        tasas["actualizacion"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        archivo.seek(0)
+        json.dump(tasas, archivo, indent=2)
+
 # Ejemplo de uso
 if __name__ == "__main__":
-    tasas = cargar_tasas("../data/tasas.json")
+    # actualizar_tasas("data/tasas.json")
+    actualizar_tasas("data/tasas.json")
+    tasas = cargar_tasas("data/tasas.json")
     precio_usd = 100.00
     precio_eur = convertir(precio_usd, "EUR", tasas)
-    registrar_transaccion("Laptop", precio_eur, "EUR", "../logs/historial.txt")
+    registrar_transaccion("Laptop", precio_eur, "EUR", "logs/historial.txt")
